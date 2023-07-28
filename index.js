722f2608eef123e785f777d121a4fead28b62112
Salvador Villa <rodavlasxp@gmail.com>
2023-07-28 16:39:16 -0400

    port

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
+const PORT = process.env.PORT || 8000;
 
 app.get('/', async (req, res) => {
 
@@ -27,7 +28,7 @@ const startServer = () => {
 
         connectDB(process.env.MONGODB_URL);
 
-        app.listen(8080, () => {
+        app.listen(PORT, () => {
             console.log('Server is running')
         })
         
